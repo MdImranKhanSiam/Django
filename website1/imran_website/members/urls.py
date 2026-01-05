19f3d34e456ca278e20212f1bf57e23d6a198b43
Imran Khan <mdimrankhansiam@gmail.com>
2026-01-06 02:52:06 +0600

    Day 3

--- website1/imran_website/members/urls.py
+++ website1/imran_website/members/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.members_func, name='homepage'),
     path('members/', views.members_func, name='members'),
-    path('members/details/<int:id>', views.details_func, name='details'),
+    path('members/details/<int:id>', views.details_func, name='Details'),
+    path('details/<int:id>', views.details_func, name='Details')
 ]
--- website1/imran_website/members/views.py
+++ website1/imran_website/members/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.shortcuts import get_object_or_404, render
 
 def members_func(request):
     mymembers = Member.objects.all().values()
@@ -10,10 +11,17 @@ def members_func(request):
     }
     return HttpResponse(template.render(context, request))
 
+# def details_func(request, id):
+#     mymember = Member.objects.get(id=id)
+#     template = loader.get_template('details.html')
+#     context = {
+#         'mymember': mymember,
+#     }
+#     return HttpResponse(template.render(context, request))
+
 def details_func(request, id):
-    mymember = Member.objects.get(id=id)
-    template = loader.get_template('details.html')
+    mymember = get_object_or_404(Member, id=id)
     context = {
         'mymember': mymember,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'details.html',context)
