ad1a677bc2a7d8c5e7564509389928c4c4401f40
Imran Khan <mdimrankhansiam@gmail.com>
2026-01-07 01:27:35 +0600

    Day 4

--- website1/imran_website/members/urls.py
+++ website1/imran_website/members/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.members_func, name='homepage'),
+    path('', views.main_func, name='homepage'),
     path('members/', views.members_func, name='members'),
     path('members/details/<int:id>', views.details_func, name='Details'),
-    path('details/<int:id>', views.details_func, name='Details')
+    path('testing/', views.testing_func, name='Testing'),
 ]
--- website1/imran_website/members/views.py
+++ website1/imran_website/members/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, render
 
 def members_func(request):
     mymembers = Member.objects.all().values()
-    template = loader.get_template('imran.html')
+    template = loader.get_template('member.html')
     context = {
         'mymembers': mymembers,
     }
@@ -25,3 +25,23 @@ def details_func(request, id):
         'mymember': mymember,
     }
     return render(request, 'details.html',context)
+
+def main_func(request):
+    user_agent = request.META.get('HTTP_USER_AGENT', 'Unknown')
+    ip_address = request.META.get('REMOTE_ADDR', 'Unknown')
+
+    context = {
+        'user_agent': user_agent,
+        'ip_address': ip_address,
+    }
+
+    return render(request, 'main.html', context)
+
+def testing_func(request):
+    fruits = ['apple','banana','mango']
+
+    context = {
+        'fruits': fruits,
+    }
+
+    return render(request, 'testing.html', context)
