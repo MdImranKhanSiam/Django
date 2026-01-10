05ca897cfe6c1599e064c258006d9e56e0a84ec1
Imran Khan <mdimrankhansiam@gmail.com>
2026-01-10 14:22:36 +0600

    Day 6

--- website1/imran_website/members/views.py
+++ website1/imran_website/members/views.py
@@ -27,21 +27,26 @@ def details_func(request, id):
     return render(request, 'details.html',context)
 
 def main_func(request):
-    user_agent = request.META.get('HTTP_USER_AGENT', 'Unknown')
-    ip_address = request.META.get('REMOTE_ADDR', 'Unknown')
-
     context = {
-        'user_agent': user_agent,
-        'ip_address': ip_address,
+
     }
 
     return render(request, 'main.html', context)
 
 def testing_func(request):
-    fruits = ['apple','banana','mango']
-
+    user_agent = request.META.get('HTTP_USER_AGENT', 'Unknown')
+    ip_address = request.META.get('REMOTE_ADDR', 'Unknown')
+    Age = 25
+    Users = Member.objects.all().values()
+    Name = 'Imran\nKhan'
+    Number = '01323401969'
     context = {
-        'fruits': fruits,
+        'Name': Name,
+        'user_agent': user_agent,
+        'ip_address': ip_address,
+        'Age': Age,
+        'Number': Number,
+        'Users': Users,
     }
 
     return render(request, 'testing.html', context)
