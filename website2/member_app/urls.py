acada2e1c3a4c7519a8e0636c39cfb730adfb577
Imran Khan <mdimrankhansiam@gmail.com>
2026-01-18 02:05:16 +0600

    Day 8

---
+++ website2/member_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main_func, name='homepage'),
+    path('members/', views.members_func, name='members'),
+    path('members/details/<int:id>', views.details_func, name='Details'),
+    path('testing/', views.testing_func, name='Testing'),
+]
---
+++ website2/member_app/views.py
@@ -0,0 +1,52 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+from django.shortcuts import get_object_or_404, render
+
+def members_func(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('member.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+# def details_func(request, id):
+#     mymember = Member.objects.get(id=id)
+#     template = loader.get_template('details.html')
+#     context = {
+#         'mymember': mymember,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+def details_func(request, id):
+    mymember = get_object_or_404(Member, id=id)
+    context = {
+        'mymember': mymember,
+    }
+    return render(request, 'details.html',context)
+
+def main_func(request):
+    context = {
+
+    }
+
+    return render(request, 'main.html', context)
+
+def testing_func(request):
+    user_agent = request.META.get('HTTP_USER_AGENT', 'Unknown')
+    ip_address = request.META.get('REMOTE_ADDR', 'Unknown')
+    Age = 25
+    Users = Member.objects.all().values()
+    Name = 'Imran\nKhan'
+    Number = '01323401969'
+    context = {
+        'Name': Name,
+        'user_agent': user_agent,
+        'ip_address': ip_address,
+        'Age': Age,
+        'Number': Number,
+        'Users': Users,
+    }
+
+    return render(request, 'testing.html', context)
